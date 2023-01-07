aba5a306b653429faec862211a54440d87c1b528
Esra KILIÇ <esrakilic18@istanbularel.edu.tr>
2023-01-07 15:53:57 +0300

    4.ders sonu 7 ocak

---
+++ genericsİntro/Program.cs
@@ -0,0 +1,13 @@
+﻿using genericsİntro;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MyList<string> isimler = new MyList<string>();
+        isimler.Add("Engin");
+
+
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ koleksiyonlar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+        Console.WriteLine(isimler2[0]);
+        Console.WriteLine(isimler2[1]);
+        Console.WriteLine(isimler2[2]);
+        Console.WriteLine(isimler2[3]);
+       
+        isimler2.Add("İlker");
+        Console.WriteLine(isimler2[4]);
+        Console.WriteLine(isimler2[0]);
+
+
+
+
+
+    }
+} 

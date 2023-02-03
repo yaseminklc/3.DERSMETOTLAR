374a846c0733d36087defe25d5a1677413bdb30b
yaseminklc <klcysmn0304@gmail.com>
2023-02-03 14:53:37 +0300

    29 cak

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product';
+
+ 
+  
+    @Component({
+      selector: 'app-product',
+      templateUrl: './product.component.html',
+      styleUrls: ['./product.component.css']
+    })
+  
+
+export class ProductComponent {
+
+  products:Product[]=[];
+}
